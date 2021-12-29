c05f36fa99441230ba17282bf96cd3248360b792
EdRiveraO <edwinrivera1810@gmail.com>
2021-12-29 17:25:14 -0500

    mejora

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ document.querySelector('.menu-btn').addEventListener('click', ()=> {
 document.querySelector('.nav-menu').classList.toggle('show');
 })
 
+document.querySelector('.showcase').addEventListener('mouseover', ()=> {
+    document.querySelector('.texto-noticia-principal').setAttribute("style", "transform: translateY(10px);opacity:100%; z-index: 2");
+    document.querySelector('.barra').setAttribute("style", "opacity:100%");
+
+    })
+
+
 ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', {delay: 500});
 ScrollReveal().reveal('.cards-banner-one', {delay: 500});
