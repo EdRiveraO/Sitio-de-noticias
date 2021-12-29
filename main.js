b12a44506810c85f01dfb3cc9d6e708f45b6eff0
EdRiveraO <edwinrivera1810@gmail.com>
2021-12-29 18:03:57 -0500

    mejora2

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ document.querySelector('.nav-menu').classList.toggle('show');
 document.querySelector('.showcase').addEventListener('mouseover', ()=> {
     document.querySelector('.texto-noticia-principal').setAttribute("style", "transform: translateY(10px);opacity:100%; z-index: 2");
     document.querySelector('.barra').setAttribute("style", "opacity:100%");
-
-    })
-
+ })
+ document.querySelector('.showcase').addEventListener('mouseout', ()=> {
+    document.querySelector('.texto-noticia-principal').setAttribute("style", "transform: translateY(50px);opacity:0%; z-index: -1");
+    document.querySelector('.barra').setAttribute("style", "opacity:0%");
+ })
 
 ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', {delay: 500});
